189328d2761b16c32ad81ff41098f2640c0bc907
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-01-19 15:47:45 +0100

    printcomb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        # This will print combinations in the desired format
-        if i == 8 and j == 9:
-            print(f"{i}{j}")
+
+for i in range(0, 9):  # First digit from 0 to 8
+    for j in range(i + 1, 10):  # Second digit from i + 1 to 9
+        if i == 8 and j == 9:  # If it's the last combination (89)
+            print("{:01d}{:01d}".format(i, j))  # Print without trailing comma
         else:
-            print(f"{i}{j}", end=", ")
+            print("{:01d}{:01d}".format(i, j), end=", ")
