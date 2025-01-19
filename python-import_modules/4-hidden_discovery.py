83bba845b3fa11a264d2a4a742521dab098e0d18
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-01-19 22:26:51 +0000

    WITo

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-import sys
-import hidden_4
-
 if __name__ == "__main__":
+    import hidden_4
+
+    # Use dir() to get all names in the module
     names = dir(hidden_4)
-    
-    filtered_names = sorted(name for name in names if not name.startswith("__"))
-    
+
+    # Filter names that do not start with '__'
+    filtered_names = [name for name in names if not name.startswith('__')]
+
+    # Sort the names in alphabetical order
+    filtered_names.sort()
+
+    # Print each name in a new line
     for name in filtered_names:
         print(name)
+
