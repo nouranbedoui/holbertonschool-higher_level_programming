e0780f7a01a216f31eb4ecd98081b92f6867629c
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-05-29 17:30:45 +0100

    rectange

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines an empty class Rectangle."""
+
+
+class Rectangle:
+    """Represents a rectangle.
+
+    This class is currently empty and can be extended to add attributes
+    and methods related to rectangles.
+    """
+    pass
