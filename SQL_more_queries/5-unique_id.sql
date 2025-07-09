bd55fa011d1ac558b4894f971e9a3d7857302270
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-07-09 17:00:16 +0100

    uniqueid

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- This script creates the table unique_id with id defaulting to 1 and unique constraint
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
