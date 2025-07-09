9e4641b9a16ba58fd31c356ad19a6615bbf30213
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-07-09 17:12:07 +0100

    citiesofcalifornia

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- List all cities of California from hbtn_0d_usa
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+AND states.name = 'California'
+ORDER BY cities.id ASC;
