705e365d37d1c7f99ece1793465224ebb01047b0
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-07-20 23:38:51 +0100

    state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define the State class and connect to the database"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that defines the structure of states table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} "
+              "<username> <password> <database>")
+        sys.exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
