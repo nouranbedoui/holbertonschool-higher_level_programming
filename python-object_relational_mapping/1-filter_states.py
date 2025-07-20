ea9565ececab8610289d4a3f6681e603894989fe
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-07-20 23:13:44 +0100

    filterstates

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Lists all states starting with N from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
