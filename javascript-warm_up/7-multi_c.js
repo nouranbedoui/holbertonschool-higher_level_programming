703fdc9cfdfe596c58f750239ecd6d786ad7c82e
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-08-05 11:30:46 +0100

    I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const args = process.argv.slice(2);
 const times = parseInt(args[0], 10);
 
-if (!times || times < 1) {
+if (!args[0]) {
   console.log('Missing number of occurrences');
-} else {
+} else if (times > 0) {
   let i = 0;
   while (i < times) {
     console.log('C is fun');
