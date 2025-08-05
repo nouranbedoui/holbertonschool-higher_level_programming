8878bf88ced15ac1aa0b9e9ed2dd42de2f806196
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-08-05 11:27:07 +0100

    Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+let output = '';
+
+while (i < languages.length) {
+  output += languages[i] + (i === languages.length - 1 ? '' : '\n');
+  i++;
+}
+
+console.log(output);
