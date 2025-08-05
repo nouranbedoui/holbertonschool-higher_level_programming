ec6cd13426d3d312ace7448086bc3e15af04ec66
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-08-05 11:32:43 +0100

    Square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const size = parseInt(args[0], 10);
+
+if (!args[0] || isNaN(size) || size <= 0) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  const line = 'X'.repeat(size);
+  while (i < size) {
+    console.log(line);
+    i++;
+  }
+}
