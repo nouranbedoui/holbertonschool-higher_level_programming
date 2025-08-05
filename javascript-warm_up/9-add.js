7a2869799d957bb5c32a4ef38eff65bf80ffe877
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-08-05 11:36:05 +0100

    Add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  const args = process.argv.slice(2);
+  const a = parseInt(args[0], 10);
+  const b = parseInt(args[1], 10);
+  
+  console.log(add(a, b));
+  
